df1af77ee2f730f1f375a571486c8deb607a8232
shallon kobusinge <kobshallon041@gmail.com>
2021-05-28 09:39:19 +0200

    Fix: added hibernate

--- src/main/java/com/example/crud_basic/utils/HibernateUtil.java
+++ src/main/java/com/example/crud_basic/utils/HibernateUtil.java
@@ -17,7 +17,7 @@ public class HibernateUtil {
 
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/dbname?characterEncoding=utf8");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/add_dbname?characterEncoding=utf8");
                 settings.put(Environment.USER, "root");
                 settings.put(Environment.PASS, "thePassword");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
